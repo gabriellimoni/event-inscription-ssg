8cfc37e7360061f0f5a9f1a62821a159be0c33f1
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-11-30 20:33:54 -0300

    adding dynamo data

--- pages/api/sign-up.js
+++ pages/api/sign-up.js
@@ -1,8 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const AWS = require('aws-sdk')
+AWS.config.credentials = new AWS.Credentials({
+  accessKeyId: process.env.AWS_APP_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_APP_SECRET_ACCESS_KEY,
+  region: process.env.AWS_APP_DEFAULT_REGION,
+})
+
+const dynamo = new AWS.DynamoDB()
+
+const EVENT_ID = 'evento_unico'
 
 export default async (req, res) => {
   console.log('LOGGING SIGNUP', req.body)
-  const { name, email, pictureFile } = req.body
+  const { name, email, pictureUrl } = req.body
   try {
     // check if email already registered
     if (await isEmailAlreadyRegistered(email)) {
@@ -11,7 +20,7 @@ export default async (req, res) => {
     }
 
     // create new register with user data
-    await doRegisterUser({ name, email, pictureFile })
+    await doRegisterUser({ name, email, pictureUrl })
 
     res.statusCode = 201
     res.end()
@@ -25,10 +34,32 @@ export default async (req, res) => {
 }
 
 async function isEmailAlreadyRegistered (email) {
-  // todo implement this
-  return false
+  const queryDataParams = {
+    TableName: 'EventRegistrations',
+    KeyConditionExpression: 'eventId = :eventId and email = :email',
+    ExpressionAttributeValues: {
+      ':eventId': { 'S': EVENT_ID },
+      ':email': { 'S': email }
+    },
+    ProjectionExpression: 'eventId, email',
+  }
+
+  const queryResult = await dynamo.query(queryDataParams).promise()
+  const items = queryResult.Items
+
+  return items.length > 0
 }
 
-async function doRegisterUser (userDate) {
-  // todo implement this
+async function doRegisterUser (userData) {
+  const putDataParams = {
+    TableName: 'EventRegistrations',
+    Item: {
+      'eventId': { 'S': EVENT_ID },
+      'email': { 'S': userData.email },
+      'name': { 'S': userData.name },
+      'pictureUrl': { 'S': userData.pictureUrl || '' },
+    }
+  }
+
+  await dynamo.putItem(putDataParams).promise()
 }
