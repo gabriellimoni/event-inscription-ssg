b4bec3453e9f1739da48ea60d402677228ad5658
gblimoni <gabriel.limoni@raizen.com>
2020-12-02 13:47:30 -0300

    adding page header

--- pages/inscription/evento_unico/[userEmail].js
+++ pages/inscription/evento_unico/[userEmail].js
@@ -1,29 +1,35 @@
 import getUserInscriptionData from '../../../lib/getUserInscriptionData'
 import Link from 'next/link'
+import Head from 'next/head'
 
 import styles from '../../../styles/Ticket.module.css'
 
 export default function EventInscription ({ userData }) {
     const { name, email, eventId, pictureUrl} = userData
     return (
-        <div className={styles.ticketWrapper}>
-            <section className={styles.ticketContainer}>
-                <div className={styles.userData}>
-                    <h1>{eventId.toUpperCase()}</h1>
-                    <h2>{name}</h2>
-                    <h3>{email}</h3>
-                </div>
-                
-                <p>
+        <>
+            <Head>
+                <title>Ticket do evento - {name}</title>
+            </Head>
+            <div className={styles.ticketWrapper}>
+                <section className={styles.ticketContainer}>
+                    <div className={styles.userData}>
+                        <h1>{eventId.toUpperCase()}</h1>
+                        <h2>{name}</h2>
+                        <h3>{email}</h3>
+                    </div>
+                    
+                    <p>
 
-                </p>
+                    </p>
 
-                <img src={pictureUrl} width="300px"/>
-            </section>
+                    <img src={pictureUrl} width="300px"/>
+                </section>
 
-            <br/>
-            <Link href="/"><a>Voltar</a></Link>
-        </div>
+                <br/>
+                <Link href="/"><a>Voltar</a></Link>
+            </div>
+        </>
     )
 }
 
