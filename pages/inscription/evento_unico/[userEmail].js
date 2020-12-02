c88fef3c157afebd609d1ab2ec99bfd79532bbc4
gblimoni <gabriel.limoni@raizen.com>
2020-12-02 12:30:55 -0300

    little design to ticker

--- pages/inscription/evento_unico/[userEmail].js
+++ pages/inscription/evento_unico/[userEmail].js
@@ -1,20 +1,29 @@
 import getUserInscriptionData from '../../../lib/getUserInscriptionData'
-
 import Link from 'next/link'
+
+import styles from '../../../styles/Ticket.module.css'
 
 export default function EventInscription ({ userData }) {
     const { name, email, eventId, pictureUrl} = userData
     return (
-        <>
-            <h1>{eventId.toUpperCase()}</h1>
-            <h2>{name}</h2>
-            <h3>{email}</h3>
-            <img src={pictureUrl} width="300px"/>
+        <div className={styles.ticketWrapper}>
+            <section className={styles.ticketContainer}>
+                <div className={styles.userData}>
+                    <h1>{eventId.toUpperCase()}</h1>
+                    <h2>{name}</h2>
+                    <h3>{email}</h3>
+                </div>
+                
+                <p>
+
+                </p>
+
+                <img src={pictureUrl} width="300px"/>
+            </section>
 
             <br/>
-            
             <Link href="/"><a>Voltar</a></Link>
-        </>
+        </div>
     )
 }
 
